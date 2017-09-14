d062caa78e87209b6e4ab6ea415ebc5b6a779098
ypj <ypj2005@126.com>
2017-09-14 22:47:19 +0800

    add post and hook group group/user relation

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,4 +1,5 @@
 class Group < ApplicationRecord
   belongs_to :user
+  has_many :posts
   validates :title, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :groups
-  
+  has_many :posts
+
 end
