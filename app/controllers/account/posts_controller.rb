7e59bb40c7748da3266c40e77ab2e242beda1520
ypj <ypj2005@126.com>
2017-09-15 18:59:01 +0800

    edit & destroy on My post page

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -1,6 +1,6 @@
 class Account::PostsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @posts = current_user.posts
+    @posts = current_user.posts.paginate(:page => params[:page], :per_page => 6)
   end
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,28 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:group_id])
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to account_posts_path, notice: "Update Success!"
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert: "Post delete!"
+  end
+
   private
 
   def post_params
